912587a25b90426d7c057745d3c9f9b583507e19
Janassalem <janamohamed248@gmail.com>
2024-09-27 14:01:02 +0300

    updated

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'janassalem$default',
+        'USER': 'janassalem',
+        'PASSWORD': '@#ExpsiJZ9:C=cR',
+        'HOST': 'janassalem.mysql.pythonanywhere-services.com',
     }
 }
+
 
 
 # Password validation
